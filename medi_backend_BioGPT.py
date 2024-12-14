9d2487339b852765c4f2bf592e3b46b5ba144e46
Ashan Lakshitha <143149942+AshanLakshitha16763@users.noreply.github.com>
2024-12-14 19:59:09 +0530

    convert to obtain 3 suggestions from model
    * num_beams=3
    * num_suggestions=3
    * max_length=15

--- medi_backend_BioGPT.py
+++ medi_backend_BioGPT.py
@@ -9,10 +9,10 @@ CORS(app)
 model_name = "microsoft/BioGPT"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
-text_gen_model = pipeline('text-generation', model=model, tokenizer=tokenizer, truncation=True, device=0) # device=0 to use GPU in mac
+text_gen_model = pipeline('text-generation', model=model, tokenizer=tokenizer, truncation=True, device=-1) # device=0 to use GPU in mac
 
 def generate_ai_suggestions(input_text, num_suggestions=1):
-    generated = text_gen_model(input_text, max_length=10, num_return_sequences=num_suggestions, num_beams=1)
+    generated = text_gen_model(input_text, max_length=15, num_return_sequences=num_suggestions, num_beams=3)
     return [g['generated_text'].strip() for g in generated]
 
 # Endpoint to check if the server is running
@@ -28,7 +28,7 @@ def suggest():
 
     if user_input:
        # common_suggestions = difflib.get_close_matches(user_input, common_phrases, n=2, cutoff=0.1)
-        ai_suggestions = generate_ai_suggestions(user_input, num_suggestions=1)
+        ai_suggestions = generate_ai_suggestions(user_input, num_suggestions=3)
         return jsonify({'suggestions': ai_suggestions}) 
 
     return jsonify({'suggestions': []})
@@ -40,4 +40,4 @@ def get_first_prompt():
     return jsonify({'prompt': "Type something here..."})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int("8000"),debug=True)
+    app.run(debug=True)
